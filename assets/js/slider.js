b9cbd93d3771e355b586e553efcceae5170f1f39
mynhhoccode <qminh775@gmail.com>
2025-09-23 08:49:50 +0700

    update slide

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
     const slides = document.querySelectorAll('.slide')
     const slidesContainer = document.querySelector('.slides')
-    const totalSlide = sildes.length
+    const totalSlide = slides.length
     let currenIndex = 1;
-    let slideWith = 100;
+    let slideWidth = 100;
     // clone
     const firstClone = slides[0].cloneNode(true)
     const lastClone = slides[totalSlide - 1].cloneNode(true)
@@ -15,20 +15,41 @@ document.addEventListener('DOMContentLoaded', function () {
     slidesContainer.insertBefore(lastClone, slidesContainer.firstChild)
 
     const allSlide = document.querySelectorAll('.slide')
-    slidesContainer.style.transform = `translateX(${slideWith * currenIndex}%)`
+    slidesContainer.style.transform = `translateX(-${slideWidth * currenIndex}%)`
 
     function show(index) {
         slidesContainer.style.transform = `translateX(-${index * 100}%)`;
     }
 
-    function nextSlide() {
-        currenIndex++
-        if (currenIndex >= sildes.length) {
-            currenIndex = 0
-        }
-        show(currenIndex)
+    function moveSlide(index) {
+        slidesContainer.style.transition = "transform: 0.8s ease-in-out"
+        slidesContainer.style.transform = `translateX(-${slideWidth * index}%)`
     }
 
-    show(currenIndex)
+    function nextSlide() {
+        currenIndex++;
+        moveSlide(currenIndex)
+    }
+
+    slidesContainer.addEventListener('transitionend', () => {
+        currentSlide = allSlide[currenIndex]
+        if (currentSlide.id === firstClone.id) {
+            slidesContainer.style.transition = `none`
+            setTimeout(() => {
+                slidesContainer.style.transition = "transform 0.8s ease-in-out";
+            });
+            currenIndex = 1
+            slidesContainer.style.transform = `translateX(-${slideWidth * currenIndex}%)`
+        }
+        if (currentSlide.id === lastClone.id) {
+            slidesContainer.style.transition = `none`
+            setTimeout(() => {
+                slidesContainer.style.transition = "transform 0.8s ease-in-out";
+            });
+            currenIndex = totalSlide
+            slidesContainer.style.transform = `translateX(-${slideWidth * currenIndex}%)`
+        }
+    })
+
     setInterval(nextSlide, 3000)
 })
