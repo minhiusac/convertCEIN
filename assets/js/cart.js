d59afa26528b865c07b14e0fa680e5649af723d9
mynhhoccode <qminh775@gmail.com>
2025-09-23 11:56:44 +0700

    update date cart.js

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -1,16 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const amout = document.querySelector(".amount")
-    const input = amout.querySelector("input")
-    const btnAdd = amout.querySelector(".btn-add")
-    const btnSubtract = amout.querySelector(".btn-subtract")
+    const amounts = document.querySelectorAll(".amount")
+    amounts.forEach(amount => {
+        const input = amount.querySelector("input")
+        const btnAdd = amount.querySelector(".btn-add")
+        const btnSubtract = amount.querySelector(".btn-subtract")
 
-    btnAdd.addEventListener('click', () => {
-        input.value = parseInt(input.value) + 1
-    })
+        btnAdd.addEventListener('click', () => {
+            input.value = parseInt(input.value) + 1
+        })
 
-    btnSubtract.addEventListener('click', () => {
-        if (parseInt(input.value) > parseInt(input.min)) {
-            input.value = parseInt(input.value) - 1
-        }
-    })
+        btnSubtract.addEventListener('click', () => {
+            if (parseInt(input.value) > parseInt(input.min)) {
+                input.value = parseInt(input.value) - 1
+            }
+        })
+    });
 })
